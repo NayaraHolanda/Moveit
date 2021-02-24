2d2188c70a0721e7ead493290d18ac8f8697196a
Nayara Holanda Moura Piauilino <naayaraholanda@gmail.com>
2021-02-23 22:44:15 -0300

    Alterar estrutura do projeto de react para nextjs

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+// Esse arquivo só carrega uma única vez. Coloco aqui tudo que é estático, que vai ser carregado por todas as páginas
